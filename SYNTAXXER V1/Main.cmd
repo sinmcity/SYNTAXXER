20dee0238075abe9aa6d185087ad916a61edb76e
sinmcity <70460242+sinmcity@users.noreply.github.com>
2021-10-29 17:36:09 -0400

    Delete SYNTAXXER V1 directory

--- SYNTAXXER V1/Main.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'Logger1.exe' with Python DLL being found.
-set PATH=c:\users\ADMINI~1\appdata\local\programs\python\python39;%PATH%
-"%~dp0.\Logger1.exe"
-
